a89d1999734c4b0d2dd48802a12057b6429772c5
Muskan Agrawal <98578119+muskanagrawal24@users.noreply.github.com>
2023-06-04 23:31:53 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var pool=require('./pool')
+
+
+/* GET home page. */
+router.post('/check_admin_login', function(req, res, next) {
+ console.log("BODY:",req.body)
+
+ pool.query("select * from admins where emailid=? and password=?",[req.body.emailaddress,req.body.password],function(error,result){
+  if(error)
+  { console.log(error)
+   return res.status(500).json({result:false}) 
+  }
+  else
+  { if(result.length==1)
+    {
+    return res.status(200).json({result:true,data:result}) }
+    else
+    {
+        return res.status(200).json({result:false}) 
+    }
+  }
+ })
+  
+});
+
+module.exports=router
---
+++ subcategory.js
@@ -0,0 +1,82 @@
+var express= require('express');
+var router= express.Router();
+var pool= require('./pool');
+var upload= require('./multer');
+
+router.post('/add_new_subcategory',upload.single('icon'),function(req,res){
+    pool.query('insert into subcategory(categoryid,subcategoryname,icon,bannerpriority) values(?,?,?,?)',[req.body.categoryid,req.body.subcategoryname,req.file.filename,req.body.priority],function(error,result){
+        if(error){
+            console.log('xxxxxxxxxxxxxxxx',error)
+            res.status(500).json({result:false})
+        }
+        else{
+            res.status(200).json({result:true})
+        }
+    })
+});
+
+router.get('/display_all_subcategory',function(req,res){
+    pool.query('select S.*,(select C.categoryname from category C where C.categoryid=S.categoryid) as cn from subcategory S',function(error,result){
+        if(error){
+            return res.status(500).json({data:''})
+        }
+        else{
+            return res.status(200).json({data:result})
+        }
+    })
+})
+
+
+router.post('/display_subcategory_by_category',function(req,res){
+    console.log("xxxxxxxxxxxx",req.body)
+    pool.query('select S.*,(select C.categoryname from category C where C.categoryid=S.categoryid) as cn from subcategory S where S.categoryid=?',[req.body.categoryid],function(error,result){
+        if(error){
+            return res.status(500).json({data:''})
+        }
+        else{
+            return res.status(200).json({data:result})
+        }
+    })
+})
+
+
+
+router.post('/edit_subcategory_data',function(req,res){
+ console.log('bodyy',req.body)
+ 
+    pool.query('update subcategory set categoryid=?,subcategoryname=?,bannerpriority=? where subcategoryid=?',
+    [req.body.categoryid,req.body.subcategoryname,req.body.bannerpriority,req.body.subcategoryid],function(error,result){
+        if(error){
+            console.log('xxxxxxxxxxxxxxxx',error)
+            res.status(500).json({status:false})
+        }
+        else{
+            res.status(200).json({status:true})
+        }
+    })
+});
+
+router.post('/delete_subcategory_data',function(req,res){
+    pool.query('delete from subcategory where subcategoryid=?',[req.body.subcategoryid],function(error,result){
+        if(error){
+            res.status(500).json({status:false})
+        }
+        else{
+            res.status(200).json({status:true})
+        }
+    })
+});
+
+router.post('/update_icon',upload.single('icon'),function(req,res){
+    pool.query('update subcategory set icon=? where subcategoryid=?',[req.file.filename,req.body.subcategoryid],function(error,result){
+        if(error){
+            res.status(500).json({status:false})
+        }
+        else{
+            res.status(200).json({status:true})
+        }
+    })
+});
+
+
+module.exports=router;
